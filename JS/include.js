af8aeaee0e298f60922549adea8a29664130ad80
SergioPerera <alu0101394503@ull.edu.es>
2023-05-15 17:54:27 +0000

    BackUp

---
+++ JS/include.js
@@ -0,0 +1,23 @@
+function includeFooter() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'footer.html', true);
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      var footer = xhr.responseText;
+      document.getElementById('footer-container').innerHTML = footer;
+    }
+  };
+  xhr.send();
+}
+
+function includeHeader() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'header.html', true);
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      var header = xhr.responseText;
+      document.getElementById('header-container').innerHTML = header;
+    }
+  };
+  xhr.send();
+}
